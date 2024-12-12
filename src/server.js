6e9c2d5d548bd9df1c00e851063b029009d1e474
porwalusa <porwalusa@gmail.com>
2024-12-12 14:19:37 -0800

    Changing welcome message

--- src/server.js
+++ src/server.js
@@ -2,7 +2,7 @@ const express = require('express')
 const app = express()
 
 app.get('/', function (req, res) {
-  res.send('Hello World')
+  res.send('Hello World. Welcome to my docker learning demo app')
 })
 
 app.listen(3000)
